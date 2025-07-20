6340d88cbb0d64732a8652f3194ffaf57dd2bf7e
Alessandro Bruno <67830217+alessandrobrunoh@users.noreply.github.com>
2025-07-20 10:36:32 +0200

    Update build-and-up.sh to stop and remove existing Docker containers before startup

--- build-and-up.sh
+++ build-and-up.sh
@@ -4,6 +4,9 @@ set -e
 echo "Building Docker image from Dockerfile..."
 docker build -t ketchapp-api .
 
+echo "Stopping and removing existing Docker containers..."
+docker-compose down --rmi all -v --remove-orphans
+
 echo "Starting Docker Compose..."
 docker-compose up --build
 
